3305c8cf2eccbacafa0a9c366932a1a919f57d97
byrkalukasz <lukasz.byrka1@gmail.com>
2021-11-04 22:57:19 +0100

    ini

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TicTacToe from './components/TicTactoe';
+import Cell from './components/Cell';
 
+Vue.component('tic-tac-toe', TicTacToe);
+Vue.component('cell', Cell);
 Vue.config.productionTip = false
 
 new Vue({
